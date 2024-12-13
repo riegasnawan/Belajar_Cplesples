a73b3fe4ab1031cb096a68acfecd309f67067d56
Riegasnawan <88017958+riegasnawan@users.noreply.github.com>
2024-12-14 01:08:45 +0700

    Add files via upload

---
+++ TipeData.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//tipe data
+	/*
+	stirng - menukliskan kalimat
+	int(integer) - bilangan bulat
+	float - bilangan pecahan
+	doublefolat = bilangan pecahan lebih banyak
+	char - character terbatas 1 huruf
+	bool - hanya memilikin 2 kondisi true or false 
+	*/
+	
+	string nama = "Riegasnawan";
+	int nomor_int = 1;
+	float nomor_float = 10.;
+	double nomor_double = 10.2;
+	char karakter = 'riegas'; //kalau ditulis "riegas" yg keluar hanya huruf s karena diambil huruf terakhir
+	bool bolean = false; //kalau true=1 sdangkan false=0
+	
+	cout<<"nama : "<<nama<<endl;
+	cout<<"int : "<<nomor_int<<endl;
+	cout<<"float : "<<nomor_float<<endl;
+	cout<<"double float : "<<nomor_double<<endl;
+	cout<<"char : "<<karakter<<endl;
+	cout<<"nbool : "<<bolean<<endl;
+} 
