3a5826431e0880cfc2fe63853b7a2e96fbac99f7
Riegasnawan <88017958+riegasnawan@users.noreply.github.com>
2024-12-14 23:52:44 +0700

    Add files via upload

---
+++ Eksplore Variable Input dan Output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string nama;
+	string asal;
+	int umur;
+	
+	cout<<"nama saya :\t";cin>>nama;   // fungsi \t biar rapih
+	cout<<"asal lahir :\t";cin>>asal;
+	cout<<"umur :";cin>>umur;cout<<endl;
+	
+	cout<<nama;cout<<" lahir di ";cout<<asal;cout<<" dan dia berumur ";cout<<umur;cout<<"tahun." ;
+}
+
